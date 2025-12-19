122dd7e364c1ce46e8d33ed5796872576e197e10
Matthew Tsai <62818122+tsaimatthew@users.noreply.github.com>
2025-12-19 14:05:51 -0800

    Fix email_user call in help form processing

--- hwapp/views.py
+++ hwapp/views.py
@@ -813,7 +813,7 @@ def helpformview(request, id):
             tracking_info = f"---------------------------------------------------------------------------------------------------------------------------------------------- \
                 <div style='display:none;color:white;font-size:0%'>@@@@tracking_id={tracking_id}@@@@</div>"
             email_user(email=helpform.email, content=f"{request.POST['message']}{tracking_info}", \
-                       subject=f"[{os.environ.get("DOMAIN_NAME")}] Help Form: {request.POST['subject']}", recipient_name=helpform.first_name)
+                       subject=f"[{os.environ.get('DOMAIN_NAME')}] Help Form: {request.POST['subject']}", recipient_name=helpform.first_name)
             helpform.status = "Completed"
             helpform.save()
             new_response = HelpForm(parent_form=helpform, first_name=request.user.first_name, last_name=request.user.last_name, \
